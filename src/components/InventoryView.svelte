5cdc74a6214a900a0705e2ba5c2dc25c3e3970ec
Pepotrouille <mechaneka83@gmail.com>
2026-01-25 22:18:01 +0100

    feat: inventory slots

--- src/components/InventoryView.svelte
+++ src/components/InventoryView.svelte
@@ -1,3 +1,21 @@
+<script lang="ts">
+    const slotNumber: number = 12; //TODO: Must depend on the Database
+</script>
+
 <div>
-    Inventory
+    <h3>Inventory</h3>
+    <div class="flex flex-wrap gap-3 pt-5 px-[15%] justify-center">
+        {#each {length: slotNumber} as _, i }
+            <div class="slot bg-base-300" id={"slot" + i}></div>
+        {/each}
+    </div>
 </div>
+
+<style>
+    .slot {
+        width: 80px;
+        height: 80px;
+        border-radius: 15%;
+        opacity: 80%;
+    }
+</style>
